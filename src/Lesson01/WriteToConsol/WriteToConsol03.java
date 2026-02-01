62c05ed452922eb2724e263357b7453fbdeb9e4e
Heescheo <Heescheo@gmail.com>
2026-02-01 23:07:57 +0100

    Med hjemmeopgave 1

--- src/Lesson01/WriteToConsol/WriteToConsol03.java
+++ src/Lesson01/WriteToConsol/WriteToConsol03.java
@@ -8,10 +8,12 @@ public class WriteToConsol03 {
 //Test comment
     public static void Message03() {
         //This is a variable.
-        String text = "Oliver";
-        int i = 47;
+        String Mission = "Mission \t Year \t Days in Space";
+        String IRISS = "IRISS \t \t 2015 \t 10";
+        String Huginn = "Huginn \t \t 2023 \t 180";
         //The value of the variable is written to the consol.
-        System.out.println(text);
-        System.out.println(i);
+        System.out.println(Mission);
+        System.out.println(IRISS);
+        System.out.println(Huginn);
     }
 }
